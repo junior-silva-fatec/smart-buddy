35dc2857daeabf0ca923f0f799262028aa6b3779
junior-silva-fatec <161388620+junior-silva-fatec@users.noreply.github.com>
2024-04-30 00:18:15 -0300

    modelo: até o item 4

---
+++ api/models/contato.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const contatoSchema = new mongoose.Schema({
+  nome: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  telefone: {
+    type: String,
+    required: true,
+  },
+  foto: {
+    type: String,
+    required: true,
+  },
+});
+
+const Contato = mongoose.model("Contato", contatoSchema);
+
+module.exports = Contato;
