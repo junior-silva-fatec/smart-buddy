86686117d40c7b6369fae4dbd19dc972adf2a79b
junior-silva-fatec <161388620+junior-silva-fatec@users.noreply.github.com>
2024-05-12 23:55:39 -0300

    login

--- smart-buddy/src/components/Header.jsx
+++ smart-buddy/src/components/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 function Header() {
   const [showModal, setShowModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleLoginClick = () => {
     setShowModal(true);
@@ -10,11 +11,42 @@ function Header() {
 
   const handleCloseModal = () => {
     setShowModal(false);
+    setErrorMessage(""); // Limpar mensagem de erro ao fechar o modal
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    try {
+      const response = await fetch("https://web-qx4yu7fnv0m1.up-us-nyc1-k8s-1.apps.run-on-seenode.com/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password })
+      });
+
+      if (response.ok) {
+        // Redirecionar para a página de eventos
+        window.location.href = "/eventos";
+      } else {
+        // Exibir mensagem de erro se a resposta não for 200
+        const data = await response.json();
+        window.location.href = "/eventos";
+        setErrorMessage(data.message || "Ocorreu um erro durante o login.");
+      }
+    } catch (error) {
+      // Exibir mensagem de erro se houver erro na requisição
+      setErrorMessage("Ocorreu um erro durante o login.");
+    }
   };
 
   return (
     <div id="header">
-      <Link to="/">
+            <Link to="/">
         <h2>Smart Buddy</h2>
       </Link>{" "}
       <button onClick={handleLoginClick}>Login</button>
@@ -25,7 +57,7 @@ function Header() {
               &times;
             </span>
             <h2>Login</h2>
-            <form>
+            <form onSubmit={handleLogin}>
               <div className="form-group">
                 <label htmlFor="email">Email:</label>
                 <input type="email" id="email" name="email" />
@@ -34,6 +66,7 @@ function Header() {
                 <label htmlFor="password">Password:</label>
                 <input type="password" id="password" name="password" />
               </div>
+              {errorMessage && <p className="error">{errorMessage}</p>}
               <button type="submit">Login</button>
             </form>
           </div>
@@ -44,3 +77,7 @@ function Header() {
 }
 
 export default Header;
+
+
+
+"https://web-qx4yu7fnv0m1.up-us-nyc1-k8s-1.apps.run-on-seenode.com/login"
