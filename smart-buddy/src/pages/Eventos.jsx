c2c5cc047d57394e57ffd57874a05fda0408dbe6
junior-silva-fatec <161388620+junior-silva-fatec@users.noreply.github.com>
2024-05-14 13:24:54 -0300

    Ícones delete e edit

--- smart-buddy/src/pages/Eventos.jsx
+++ smart-buddy/src/pages/Eventos.jsx
@@ -1,18 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
+//require("dotenv/config");
 
 function Eventos() {
   const [events, setEvents] = useState([]);
   const [selectedEvent, setSelectedEvent] = useState(null);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
+  const urlEventos = "http://localhost:80/events";
+  //const urlEventos = "https://web-qx4yu7fnv0m1.up-us-nyc1-k8s-1.apps.run-on-seenode.com/events"
+
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await fetch(
-          "https://web-qx4yu7fnv0m1.up-us-nyc1-k8s-1.apps.run-on-seenode.com/events"
-        );
+        const response = await fetch(urlEventos);
         if (response.ok) {
           const data = await response.json();
           setEvents(data);
@@ -29,14 +34,11 @@ function Eventos() {
 
   const handleDeleteEvent = async (eventId) => {
     try {
-      const response = await fetch(
-        `https://web-qx4yu7fnv0m1.up-us-nyc1-k8s-1.apps.run-on-seenode.com/events/${eventId}`,
-        {
-          method: "DELETE",
-        }
-      );
+      const response = await fetch(`${urlEventos}/${eventId}`, {
+        method: "DELETE",
+      });
       if (response.ok) {
-        setEvents(events.filter(event => event._id !== eventId));
+        setEvents(events.filter((event) => event._id !== eventId));
         console.log("Event deleted successfully");
       } else {
         console.error("Failed to delete event:", response.statusText);
@@ -48,18 +50,19 @@ function Eventos() {
 
   const handleEditEvent = async (updatedEvent) => {
     try {
-      const response = await fetch(
-        `https://web-qx4yu7fnv0m1.up-us-nyc1-k8s-1.apps.run-on-seenode.com/events/${updatedEvent._id}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(updatedEvent)
-        }
-      );
+      const response = await fetch(`${urlEventos}/${updatedEvent._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedEvent),
+      });
       if (response.ok) {
-        setEvents(events.map(event => (event._id === updatedEvent._id ? updatedEvent : event)));
+        setEvents(
+          events.map((event) =>
+            event._id === updatedEvent._id ? updatedEvent : event
+          )
+        );
         setIsEditModalOpen(false);
         console.log("Event updated successfully");
       } else {
@@ -88,7 +91,12 @@ function Eventos() {
 
         <div className="events-list">
           {events.map((event) => (
-            <Event key={event._id} event={event} onDelete={() => handleDeleteEvent(event._id)} onEdit={() => openEditModal(event)} />
+            <Event
+              key={event._id}
+              event={event}
+              onDelete={() => handleDeleteEvent(event._id)}
+              onEdit={() => openEditModal(event)}
+            />
           ))}
         </div>
 
@@ -107,7 +115,18 @@ function Eventos() {
 const Event = ({ event, onDelete, onEdit }) => {
   const diaDoMes = Number(event.date.substring(8, 10));
   const nomesMes = [
-    "JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ"
+    "JAN",
+    "FEV",
+    "MAR",
+    "ABR",
+    "MAI",
+    "JUN",
+    "JUL",
+    "AGO",
+    "SET",
+    "OUT",
+    "NOV",
+    "DEZ",
   ];
   const mes = nomesMes[Number(event.date.substring(5, 7)) - 1];
 
@@ -124,8 +143,12 @@ const Event = ({ event, onDelete, onEdit }) => {
       <div className="colDescricaoEvento">
         <h2>{event.title}</h2>
         <p>{event.description}</p>
-        <button onClick={onDelete}>Delete</button>
-        <button onClick={onEdit}>Edit</button>
+        <button className="botaoDelete" onClick={onDelete}>
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
+        <button className="botaoEdit" onClick={onEdit}>
+          <FontAwesomeIcon icon={faPencil} />
+        </button>
       </div>
     </div>
   );
@@ -151,26 +174,53 @@ const EditEventModal = ({ event, onClose, onSave }) => {
         <form onSubmit={handleSubmit}>
           <label>
             Title:
-            <input type="text" name="title" value={formData.title} onChange={handleChange} />
+            <input
+              type="text"
+              name="title"
+              value={formData.title}
+              onChange={handleChange}
+            />
           </label>
           <label>
             Description:
-            <input type="text" name="description" value={formData.description} onChange={handleChange} />
+            <input
+              type="text"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+            />
           </label>
           <label>
             Date:
-            <input type="date" name="date" value={formData.date} onChange={handleChange} />
+            <input
+              type="date"
+              name="date"
+              value={formData.date}
+              onChange={handleChange}
+            />
           </label>
           <label>
             Time:
-            <input type="time" name="time" value={formData.time} onChange={handleChange} />
+            <input
+              type="time"
+              name="time"
+              value={formData.time}
+              onChange={handleChange}
+            />
           </label>
           <label>
             Duration (hours):
-            <input type="number" name="duration" value={formData.duration} onChange={handleChange} />
+            <input
+              type="number"
+              name="duration"
+              value={formData.duration}
+              onChange={handleChange}
+            />
           </label>
           <button type="submit">Save</button>
-          <button type="button" onClick={onClose}>Cancel</button>
+          <button type="button" onClick={onClose}>
+            Cancel
+          </button>
         </form>
       </div>
     </div>
